850af4796a0edadb9a3b442f9b2e7ef0667a986a
Kevin <kevin.castro.chavez0@gmail.com>
2023-01-03 14:35:48 -0700

    created the options file to pass default options when chatbot talks

--- src/chatbot/config.js
+++ src/chatbot/config.js
@@ -1,8 +1,20 @@
 import { createChatBotMessage } from 'react-chatbot-kit';
 
+import Options from '../components/Options/Options';
+
 const config = {
   botName: 'Learning bot',
-  initialMessages: [createChatBotMessage('Hello World')],
+  initialMessages: [
+    createChatBotMessage('Hello, what do you want to learn', {
+      widget: 'options',
+    }),
+  ],
+  widgets: [
+    {
+      widgetName: 'options',
+      widgetFunc: (props) => <Options {...props} />,
+    },
+  ],
 };
 
 export default config;
